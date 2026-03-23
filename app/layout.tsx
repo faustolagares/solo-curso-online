69a86814f52831a82efd736f6a3b68df6d2f30a8
Ruan <ruanpablomp@gmail.com>
2026-03-23 14:59:34 -0300

    add pixel meta code

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,8 +36,33 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
             })(window, document, "clarity", "script", "vz0gj9t8ti");
           `}
         </Script>
+
+         <Script id="pixel-meta" strategy="afterInteractive">
+          {`
+            
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1552487645813151');
+            fbq('track', 'PageView');            
+          `}
+        </Script>
+
+      
       </head>
-      <body className="font-sans antialiased bg-white text-gray-900" suppressHydrationWarning>{children}</body>
-    </html>
+      <body className="font-sans antialiased bg-white text-gray-900" suppressHydrationWarning>
+        <noscript>
+          <img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=1552487645813151&ev=PageView&noscript=1"
+            />
+        </noscript>
+        {children}
+      </body>
+      </html>
   );
 }
